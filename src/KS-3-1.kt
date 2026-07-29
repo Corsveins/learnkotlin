6d350082a2002f1098ec60812034acd72db9c44f
Валерий <mcmakar056@gmail.com>
2026-07-29 18:37:08 +0300

    KS-3-1 Приветствие в приложении

--- src/KS-3-1.kt
+++ src/KS-3-1.kt
@@ -2,11 +2,10 @@ fun main () {
 
     val userName = "Валерий"
     var greetingsUser = "Доброе утро"
-    println("${greetingsUser + ", " + userName + "."}")
 
-    greetingsUser = "Добрый день"
     println("${greetingsUser + ", " + userName + "."}")
 
     greetingsUser = "Доброй ночи"
+
     println("${greetingsUser + ", " + userName + "."}")
 }
