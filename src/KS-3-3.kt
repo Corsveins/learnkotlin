31f2401eae0ab202d698f12e57b9a1bd8284cf0c
Валерий <mcmakar056@gmail.com>
2026-07-31 16:34:05 +0300

    KS-3-3 String templates — таблица умножения

--- src/KS-3-3.kt
+++ src/KS-3-3.kt
@@ -2,14 +2,14 @@ fun main() {
     val numberUser = 6
 
     println("""
-        |$numberUser * 1 = ${numberUser * 1}
-        |$numberUser * 2 = ${numberUser * 2}
-        |$numberUser * 3 = ${numberUser * 3}
-        |$numberUser * 4 = ${numberUser * 4}
-        |$numberUser * 5 = ${numberUser * 5}
-        |$numberUser * 6 = ${numberUser * 6}
-        |$numberUser * 7 = ${numberUser * 7}
-        |$numberUser * 8 = ${numberUser * 8}
-        |$numberUser * 9 = ${numberUser * 9}
+        |$numberUser x 1 = ${numberUser * 1}
+        |$numberUser x 2 = ${numberUser * 2}
+        |$numberUser x 3 = ${numberUser * 3}
+        |$numberUser x 4 = ${numberUser * 4}
+        |$numberUser x 5 = ${numberUser * 5}
+        |$numberUser x 6 = ${numberUser * 6}
+        |$numberUser x 7 = ${numberUser * 7}
+        |$numberUser x 8 = ${numberUser * 8}
+        |$numberUser x 9 = ${numberUser * 9}
     """.trimMargin())
 }
