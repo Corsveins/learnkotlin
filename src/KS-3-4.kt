dd0e304018b54a702e628edb1d61d9d4083a7686
Валерий <mcmakar056@gmail.com>
2026-07-31 17:48:09 +0300

    KS-3-4 Методы строки — ход в шахматах

---
+++ src/KS-3-4.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var whereFrom = "E2"
+    var where = "E4"
+    var strokenNumber = 1
+    println("[$whereFrom - $where,$strokenNumber]")
+
+    whereFrom = "D2"
+    where = "D3"
+    strokenNumber = 1
+
+    println("[$whereFrom - $where,$strokenNumber]")
+}
