9d833cab51a733fa7460d96c9a389c51682e8471
Валерий <mcmakar056@gmail.com>
2026-08-04 09:26:15 +0300

    KS-4-2 Равенство — таможенный терминал

--- src/KS-4-2.kt
+++ src/KS-4-2.kt
@@ -6,9 +6,11 @@ const val CAPACITY_AVERAGE = 100
 fun main() {
     val startCargo = 20
     val capacity = 80
-    println("Груз с весом $startCargo кг и объемом $capacity л соответствует категории 'Average': ${(startCargo > START_WEIGHT && startCargo <= END_WEIGHT) && (capacity < CAPACITY_AVERAGE)}")
+    println("Груз с весом $startCargo кг и объемом $capacity л соответствует категории 'Average': ${(startCargo > START_WEIGHT && startCargo <= END_WEIGHT) && 
+            (capacity < CAPACITY_AVERAGE)}")
 
     val twoCargo = 50
     val twoCapacity = 100
-    println("Груз с весом $twoCargo кг и объемом $twoCapacity л соответствует категории 'Average': ${(twoCargo > START_WEIGHT && twoCargo <= END_WEIGHT) && (twoCapacity < CAPACITY_AVERAGE)}")
+    println("Груз с весом $twoCargo кг и объемом $twoCapacity л соответствует категории 'Average': ${(twoCargo > START_WEIGHT && twoCargo <= END_WEIGHT) && 
+            (twoCapacity < CAPACITY_AVERAGE)}")
 }
