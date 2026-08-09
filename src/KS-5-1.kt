f061a35c0d2b225a434e7e802abe9eb327c1488a
Валерий <mcmakar056@gmail.com>
2026-08-09 17:52:30 +0300

    KS-5-1 if — авторизация против ботов

--- src/KS-5-1.kt
+++ src/KS-5-1.kt
@@ -6,7 +6,7 @@ fun main() {
 
     println("Введите результат сложения чисел $oneNumber + $twoNumber = ")
     val userInput = readln().toIntOrNull()
-    if(validResult == userInput) {
+    if (validResult == userInput) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещен.")
