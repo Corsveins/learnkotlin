b7faeb2b044001194c10b56b825beb296b2d9e12
Валерий <mcmakar056@gmail.com>
2026-08-10 11:08:14 +0300

    KS-5-2 if-else — проверка совершеннолетия

---
+++ src/KS-5-2.kt
@@ -0,0 +1,14 @@
+const val VALID_AGE = 18
+fun main() {
+
+    println("Введите год своего рождения:")
+    val userYear: Int = readln().toInt()
+    val todayYear = 2026
+    val reserchYear = todayYear - userYear
+
+    if (reserchYear >= VALID_AGE) {
+        println("Показать экран со скрытым контентом")
+    } else {
+        println("Отказать в доступе")
+    }
+}
