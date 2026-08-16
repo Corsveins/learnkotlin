a5b0f9ecb0568e2ee711914f6e40216346bc3d84
mcmakar056 <mcmakar056@xmail.ru>
2026-08-16 18:15:08 +0300

    KS-6-2 Счётчик while — консольный таймер

--- src/KS-6-2.kt
+++ src/KS-6-2.kt
@@ -3,9 +3,9 @@ fun main() {
     println("Введите количество секунд:")
     val countSeconds = readln().toInt()
     var count = 0
-    while (count <= countSeconds) {
+    while (count < countSeconds) {
         Thread.sleep(1000)
         count++
     }
-    println("Прошло $countSeconds секундccc")
+    println("Прошло $countSeconds секунд")
 }
