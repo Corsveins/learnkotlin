d942b760bf86487a3a5a700ac0b15aa34b0fb6a1
mcmakar056 <mcmakar056@xmail.ru>
2026-08-16 13:51:23 +0300

    KS-6-1 while — авторизация в приложении

--- src/KS-6-1.kt
+++ src/KS-6-1.kt
@@ -1,3 +1,17 @@
 fun main() {
 
+    println("Задайте логин:")
+    val login = readln()
+    println("Задайте пароль:")
+    val password = readln()
+
+    println("Введите логин:")
+    val inputLogin = readln()
+    println("Введите пароль:")
+    val inputPassword = readln()
+
+    while (login != inputLogin || password != inputPassword) {
+        println("Неверный логин ил пароль повторите попытку")
+    }
+    println("Авторизация прошла успешно")
 }
