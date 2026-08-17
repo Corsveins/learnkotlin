c4868505fbadf26494f1e1175efed0386087bffc
Валерий <mcmakar056@gmail.com>
2026-08-17 16:07:58 +0300

    KS-6-3 do-while — таймер с обратным отсчётом

--- src/KS-6-3.kt
+++ src/KS-6-3.kt
@@ -3,7 +3,7 @@ fun main() {
     println("Введите количество секунд:")
     var inputSeconds = readln().toInt()
 
-    while (inputSeconds > 0){
+    while (inputSeconds > 0) {
         println("Осталось секунд: $inputSeconds")
         inputSeconds--
         Thread.sleep(1000)
