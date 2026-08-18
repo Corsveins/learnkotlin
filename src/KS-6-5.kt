3032a2837306c062d9ebb4afec477d91bba7e1ce
Валерий <mcmakar056@gmail.com>
2026-08-18 15:09:20 +0300

    KS-6-5 continue — капча с попытками

--- src/KS-6-5.kt
+++ src/KS-6-5.kt
@@ -3,13 +3,13 @@ import kotlin.random.nextInt
 fun main() {
 
 
-    var countAttepts = 3
+    var attemptsLeft = 3
     /*
     * Доработка задачи на авторизацию. Нужно написать часть модуля для авторизации пользователя. Для входа в приложение пользователь должен доказать,
     * что он не бот. Для этого программа предлагает решить простой математический пример – сложить два числа.
     * Имитируй эти действия в консоли, путем считывания данных с клавиатуры.
     * */
-    while (countAttepts > 0){
+    while (attemptsLeft > 0) {
         val randomNumber = Random.nextInt(1..9)
         val randomTwo = Random.nextInt(1..9)
         val allResult = randomNumber + randomTwo
@@ -21,7 +21,7 @@ fun main() {
         } else {
             println("Неверно повторите попытку!")
         }
-        countAttepts--
+        attemptsLeft--
     }
     println("Доступ запрещен")
 }
