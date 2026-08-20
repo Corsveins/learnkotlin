5454545a7731e652e2862bd12357e18f9f664ca1
Валерий <mcmakar056@gmail.com>
2026-08-20 17:56:22 +0300

    KS-7-2 downTo — авторизация по SMS

--- src/KS-7-2.kt
+++ src/KS-7-2.kt
@@ -7,11 +7,12 @@ fun main (){
     var userInput: Int
 
     do {
-        code = Random.nextInt(1000 .. 9999)
+        code = Random.nextInt(1000..9999)
         println("Ваш код авторизации: $code")
         println("Введите код авторизации:")
         userInput = readln().toInt()
-        if(userInput != code) {
+
+        if (userInput != code) {
             println("код введён неверно")
         }
     } while (userInput != code)
