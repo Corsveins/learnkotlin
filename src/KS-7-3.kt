7070d334755cd1cad3ec42b9ad95f1adee2ac379
Валерий <mcmakar056@gmail.com>
2026-08-21 12:01:16 +0300

    KS-7-3 until — чётные числа

--- src/KS-7-3.kt
+++ src/KS-7-3.kt
@@ -2,7 +2,7 @@ fun main() {
 
     println("Введите число:")
     val userInput = readln().toInt()
-    for (i in 0..userInput step 2){
+    for (i in 0..userInput step 2) {
         println(i)
     }
 }
