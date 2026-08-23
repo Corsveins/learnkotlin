612b495298c8f4ed75a634667025f3f74af2c092
mcmakar056 <mcmakar056@xmail.ru>
2026-08-23 18:45:12 +0300

    KS-8-1: arrayOf — статистика просмотров

--- src/KS-8-1.kt
+++ src/KS-8-1.kt
@@ -1,7 +1,7 @@
 fun main() {
 
-    val totalViews = arrayOf(1,2,3,4,7,9,9)
-    var sum= 0
+    val totalViews = arrayOf(1, 2, 3, 4, 7, 9, 9)
+    var sum = 0
 
     totalViews.forEach {
         sum += it
