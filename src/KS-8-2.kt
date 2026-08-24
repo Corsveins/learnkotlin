aa2947e327a6e37867424051e68da06c386f5626
Валерий <mcmakar056@gmail.com>
2026-08-24 12:50:39 +0300

    KS-8-2: Доступ к элементам — рецепт

---
+++ src/KS-8-2.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val ingredients = arrayOf("Картошка", "Свекла", "Лук")
+
+    println("Выберите ингредиент:")
+    val userInput = readln().trim()
+
+    for (item in ingredients) {
+        if (userInput.equals(item, ignoreCase = true)) {
+            println("Ингредиент $item в рецепте есть")
+            return
+        }
+    }
+    println("Такого ингредиента в рецепте нет")
+}
