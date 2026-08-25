12dbb5238b3091f4331834966bdb3e46fdf09f31
Валерий <mcmakar056@gmail.com>
2026-08-25 10:43:54 +0300

    KS-8-3: Поиск в массиве — рецепт без цикла

---
+++ src/KS-8-3.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val ingredients = arrayOf("Картошка", "Свекла", "Лук")
+
+    println("Выберите ингредиент:")
+    val userInput = readln().trim()
+
+    if (ingredients.any {it.equals(userInput, ignoreCase = true) }) {
+        println("Ингредиент $userInput в рецепте есть")
+    } else {
+        println("Такого ингредиента в рецепте нет")
+    }
+
+}
